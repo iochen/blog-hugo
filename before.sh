bf08d43172e5783d30af0c88f9588c3899eb3129
Richard Chen <i@iochen.com>
2020-07-16 10:59:53 +0800

    add cdn

--- before.sh
+++ before.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# for local search
 go get -v github.com/iochen/local-search/cmd/localgen/
-
 localgen -p ./content/post/ -o ./content/search/search.json -d "/post/" -l -c
+
+# for cdn
+sed -i '/^#.*FOR_CDN/s/^#//' config.toml
